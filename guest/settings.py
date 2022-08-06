2c1a0ceb15491f9993209ecb427fde691ad8d426
myComputerWin11 <myComputerWin11@git.com>
2022-08-06 11:03:23 +0800

    update: Django配置MySQL 已经迁移Mysql

--- guest/settings.py
+++ guest/settings.py
@@ -73,8 +73,15 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'guest',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
